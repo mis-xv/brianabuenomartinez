892dd3f1c333d75330398bd671ed57285ac38383
mis-xv <kala980108@gmail.com>
2026-05-09 06:22:04 -0600

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuenta', {
 	year: 2026, // required
 	month: 7, // required
 	day: 17, // required
-	hours: 13, // Default is 0 [0-23] integer
+	hours: 10, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
